535d176f1b876ac78949d925da94eb194840d963
Gabriele Crivellari <gabriele.crivellari@itcoregroup.com>
2023-08-18 12:07:55 +0200

    migration release 2

---
+++ src/Lacos.GestioneCommesse.Dal/Migrations/20230818075037_release2.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Lacos.GestioneCommesse.Dal.Migrations
+{
+    /// <inheritdoc />
+    public partial class release2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Description",
+                schema: "Registry",
+                table: "OperatorDocuments",
+                newName: "OriginalFilename");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Email",
+                schema: "Registry",
+                table: "Customers",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Telephone",
+                schema: "Registry",
+                table: "Customers",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Email",
+                schema: "Registry",
+                table: "Customers");
+
+            migrationBuilder.DropColumn(
+                name: "Telephone",
+                schema: "Registry",
+                table: "Customers");
+
+            migrationBuilder.RenameColumn(
+                name: "OriginalFilename",
+                schema: "Registry",
+                table: "OperatorDocuments",
+                newName: "Description");
+        }
+    }
+}

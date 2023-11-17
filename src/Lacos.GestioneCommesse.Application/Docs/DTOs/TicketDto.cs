9ce8d35e73edb11853f3d92540cfa267ee7196ef
Paolo Bellotti <paolo.bellotti@tethasolutions.com>
2023-11-17 11:42:30 +0100

    modifiche rilascio

--- src/Lacos.GestioneCommesse.Application/Docs/DTOs/TicketDto.cs
+++ src/Lacos.GestioneCommesse.Application/Docs/DTOs/TicketDto.cs
@@ -1,6 +1,5 @@
 ﻿using Lacos.GestioneCommesse.Application.Shared;
 using Lacos.GestioneCommesse.Domain.Docs;
-using Lacos.GestioneCommesse.Domain.Registry;
 
 namespace Lacos.GestioneCommesse.Application.Docs.DTOs;
 

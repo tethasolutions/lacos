f8fb5ae262dc0e2cd3d7470f4d278a912ada10bd
Gabriele Crivellari <gabriele.crivellari@itcoregroup.com>
2024-03-29 09:44:56 +0100

    Modifiche SyncDtos

--- src/Lacos.GestioneCommesse.Contracts/Dtos/Docs/SyncJobDto.cs
+++ src/Lacos.GestioneCommesse.Contracts/Dtos/Docs/SyncJobDto.cs
@@ -9,6 +9,7 @@ namespace Lacos.GestioneCommesse.Contracts.Dtos.Docs
         public int? Number { get; set; }
         public int? Year { get; set; }
         public DateTimeOffset? JobDate { get; set; }
+        public DateTimeOffset? ExpirationDate { get; set; }
         public string? Description { get; set; }
         public string? Reference { get; set; }
         public bool HasHighPriority { get; set; }
--- src/Lacos.GestioneCommesse.Contracts/Dtos/Docs/SyncPurchaseOrderDto.cs
+++ src/Lacos.GestioneCommesse.Contracts/Dtos/Docs/SyncPurchaseOrderDto.cs
@@ -8,12 +8,12 @@ namespace Lacos.GestioneCommesse.Contracts.Dtos.Docs
         public int? Number { get; set; }
         public int? Year { get; set; }
         public DateTimeOffset? Date { get; set; }
+        public DateTimeOffset? ExpectedDate { get; set; }
         public string? Description { get; set; }
         public PurchaseOrderStatus? Status { get; set; }
         public long? JobId { get; set; }
         public long? SupplierId { get; set; }
         public long? OperatorId { get; set; }
-
     }
     
 }

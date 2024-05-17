c56f3131f1438ad38583bc38b443654d385d65f2
Gabriele Crivellari <gabriele.crivellari@itcoregroup.com>
2024-05-17 18:37:39 +0200

    Ripristinato Program.cs

--- src/Lacos.GestioneCommesse.WebApi/Program.cs
+++ src/Lacos.GestioneCommesse.WebApi/Program.cs
@@ -1,12 +1,12 @@
 using Lacos.GestioneCommesse.WebApi;
 
 var builder = WebApplication.CreateBuilder(args);
-#if DEBUG 
-builder.WebHost.UseKestrel();
-builder.WebHost.UseIIS();
-builder.WebHost.UseUrls("http://*:37998");
-#else
-#endif
+//#if DEBUG 
+//builder.WebHost.UseKestrel();
+//builder.WebHost.UseIIS();
+//builder.WebHost.UseUrls("http://*:37998");
+//#else
+//#endif
 
 
 builder.Services.AddWebApi(builder.Configuration);

76c802ee590b5caa13c235c4d7d253ef8c4ff718
Paolo Bellotti <paolo.bellotti@tethasolutions.com>
2025-01-08 18:00:01 +0100

    rimozione parte commentata

--- src/Lacos.GestioneCommesse.WebApi/Controllers/SharepointController.cs
+++ src/Lacos.GestioneCommesse.WebApi/Controllers/SharepointController.cs
@@ -12,14 +12,6 @@ public class SharepointController : LacosApiController
     {
         using (var http = new HttpClient())
         {
-            //var uri = "https://accounts.accesscontrol.windows.net/136f579a-1be2-44ab-a6de-3d19af66d603/tokens/OAuth/2";
-            //var values = new List<KeyValuePair<string, string>>()
-            //    {
-            //        new KeyValuePair<string, string>("grant_type", "client_credentials"),
-            //        new KeyValuePair<string, string>("client_id", "eb6471b1-0c2c-412b-9b8b-025afc733a4c@136f579a-1be2-44ab-a6de-3d19af66d603"),
-            //        new KeyValuePair<string, string>("client_secret", "G258Q~sEZhWSnwb4_myUOZoJDWzsRTaP6jg~rbS2"),
-            //        new KeyValuePair<string, string>("resource", "00000003-0000-0ff1-ce00-000000000000/tetha365.sharepoint.com@136f579a-1be2-44ab-a6de-3d19af66d603")
-            //    };
             var uri = "https://login.microsoftonline.com/b65690a8-45a2-4630-b821-a331c42268c9/oauth2/v2.0/token";
             var values = new List<KeyValuePair<string, string>>()
                 {
